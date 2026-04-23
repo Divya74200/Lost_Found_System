ad99d7cfea6d06d31e6a1247bd54aad66fcd983b
Divya74200 <divya.2428cseai2164@kiet.edu>
2026-04-23 12:23:44 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "baseURL: "https://your-backend-url.onrender.com/api"",
 });
 
 // Attach token automatically
