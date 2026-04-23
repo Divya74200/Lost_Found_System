6e9aac967daa16cab4420d55741ade378d663840
Divya74200 <divya.2428cseai2164@kiet.edu>
2026-04-23 14:18:34 +0530

    fix username in controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 // Register
 exports.register = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { username, email, password } = req.body;
 
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: "User already exists" });
